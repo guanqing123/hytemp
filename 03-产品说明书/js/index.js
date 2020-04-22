3269833893a51c3666678ea09c9562a2fc453b20
gq <guanqinghz@163.com>
2020-04-22 10:30:02 +0800

    产品说明书完结

--- 03-产品说明书/js/index.js
+++ 03-产品说明书/js/index.js
@@ -7,10 +7,10 @@ define(['config', 'vue', 'plugins/installer', 'plugins/PullUpDown','mui', 'jquer
         },
         template: `<div class="mui-content">
              <div class="mui-content-padded" style="margin: 15px 5px 0px 5px">
-                 <div class="mui-input-row mui-search">
-                     <input type="search" class="mui-input-clear customW" placeholder="产品型号" @blur="search">
-                     <button type="button" >搜索</button>
+                 <div class="mui-input-row mui-search customW">
+                     <input type="search" class="mui-input-clear" placeholder="产品型号" @blur="search">
                  </div>
+                 <button type="button" >搜索</button>
              </div>
              <ul class="mui-table-view mui-table-view-striped mui-table-view-condensed pull">
                 <PullUpDown ref="pull" :pullDown="false" :sum="sum" :currentPage="currentPage" :count="count" @nextPage="nextPage">
@@ -42,8 +42,9 @@ define(['config', 'vue', 'plugins/installer', 'plugins/PullUpDown','mui', 'jquer
                 <span class="mui-icon mui-icon-arrowup"></span>
              </a>
             
-             <div class="mui-zoom-scroller" v-show="showImg" @click="hideImg">
+             <div class="mui-zoom-scroller" v-show="showImg">
                 <img :src="src" data-preview-lazyload="" style="" class="mui-zoom" @load="loadImage">
+                <button type="button" @click="hideImg" class="close">X</button>
              </div>
         </div>`,
         data: function () {
@@ -125,10 +126,13 @@ define(['config', 'vue', 'plugins/installer', 'plugins/PullUpDown','mui', 'jquer
             download: function (wjm) {
                 var self = this;
                 if (!self.$isImg(wjm.wjm)) {
+                    // window.location.href = 'download.html?wjm='+ wjm.wjm
+                    // return false;
                     var btns = ['否', '下载'];
                     mui.confirm('非图片格式文件,是否下载?', '温馨提醒', btns, function (e) {
                         if (e.index == 1) {
-                            window.location.href = 'download.html?wjm='+ wjm.wjm
+                            window.location.href = 'download.html?wjm='+ wjm.wjm;
+                            return false;
                         }
                     })
                     return false;
